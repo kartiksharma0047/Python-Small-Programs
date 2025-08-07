7d6b4b0487625d84b1d845970f94b89ed077d16e
Kartik Sharma <129365236+kartiksharma0047@users.noreply.github.com>
2025-08-07 12:09:50 +0530

    Completed Dumbell Pattern
    -> Time Complexity is also improved ( O(n) )
    -> Dynamic Handling of rows and columns
    Note - rows and columns should be even in numbers and rows should be less than column to avoid issues and if row is equal to column then we get rhombus pattern

--- Pattern Printing/Dumbell_Shape.py
+++ Pattern Printing/Dumbell_Shape.py
@@ -6,20 +6,37 @@
 #    * * * * * * * * * * * * * * *
 #      * *                   * *
 #        *                   *
-Rows=6
-Columns=14
 
 def printChar(is_star):
     print("* " if is_star else "  ",end="")
 
-for row in range(Rows):
-    for col in range(Columns):
-        if row in [0,5]:
-            printChar(col in [2,11])
-        elif row in [1,4]:
-            printChar(col in [1,2,11,12])
-        elif row in [2,3]:
-            printChar(True)
+# HardCoded Logic
+# for row in range(Rows):
+#     for col in range(Columns):
+#         if row in [0,5]:
+#             printChar(col in [2,11])
+#         elif row in [1,4]:
+#             printChar(col in [1,2,11,12])
+#         elif row in [2,3]:
+#             printChar(True)
+#         else:
+#             printChar(False)
+#     print()
+
+# Dynamic Logic
+# Even Rows and Even Columns
+Rows=10
+Columns=30
+mid=int(Rows/2)
+mid_distance=int(Columns-Rows)
+for row in range(1,Rows+1):
+    if row>=0 and row<=mid:
+        if row==mid:
+            print("*"*(mid_distance+(mid*2)))
         else:
-            printChar(False)
-    print()
+            print(" "*(mid-row)+"*"*row+" "*mid_distance+"*"*row)
+    else:
+        if row>mid and row==mid+1:
+            print("*"*(mid_distance+(mid*2)))
+        else:
+            print(" "*(row-mid-1)+"*"*(Rows-row+1)+" "*mid_distance+"*"*(Rows-row+1))
